dfb1929a610a9d9f6eadae458ab07aa106c0896d
9thea9 <100798115+9thea9@users.noreply.github.com>
2022-03-02 20:05:24 +0100

    Initial commit

---
+++ metabolism.R
@@ -0,0 +1,65 @@
+##group projects
+##djabarov, kernbichler, schwingshackl, weninger
+##code by TS
+
+#set the right working directory on your PC
+setwd("C:/Users/Thea/Desktop/vu_datenanalyse/Group project")
+
+#load the needed packages
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(patchwork)
+
+#import the dataset 
+metabolism<-read_excel("Data_Metabolism.xlsx", sheet="data")%>%
+  select(2,3, 8:27)
+
+
+##First focus on the main hypothesis (provided by the plot)
+#including: Nutriente, landuse, light, DOC and ER
+#check normal distribution - still looking for nicer way to do this  
+h1<-ggplot(data=metabolism, aes(x=Agr))+geom_histogram(bins=10)#looks good
+h2<-ggplot(data=metabolism, aes(x=light))+geom_histogram(bins=10)#transformation
+h3<-ggplot(data=metabolism, aes(x=ER))+geom_histogram(bins=10)#transformation
+h4<-ggplot(data=metabolism, aes(x=SRP))+geom_histogram(bins=10)#transformation
+h5<-ggplot(data=metabolism, aes(x=DIN))+geom_histogram(bins=10)#looks good
+h6<-ggplot(data=metabolism, aes(x=DOC))+geom_histogram(bins=12)#transformation
+
+(h1+h2+h3)/(h4+h5+h6)
+
+#log transform data, that is not normally distributed
+metabolism$log_light<-log(metabolism$light)
+metabolism$log_ER<-log(metabolism$ER *-1) #befor log(), all values need to be positive
+metabolism$log_SRP<-log(metabolism$SRP)
+metabolism$log_DOC<-log(metabolism$DOC)
+
+#check again
+hist(metabolism$log_DOC)
+hist(metabolism$log_ER)
+hist(metabolism$log_light)#still not so good 
+hist(metabolism$log_SRP)
+
+#shapiro wilk test for more clarity
+shapiro.test(metabolism$Agr) #p<1
+shapiro.test(metabolism$log_light)#p<0.5
+shapiro.test(metabolism$log_ER)#p<0.5
+shapiro.test(metabolism$log_SRP)#p<0.5
+shapiro.test(metabolism$log_DOC)#p<1
+shapiro.test(metabolism$DIN)#p<0.05
+
+
+#subset &plot the dataset 
+met_sub<-select(metabolism, c(Agr, log_light, log_ER, log_SRP, log_DOC, DIN))
+plot(met_sub)
+
+#building a linear model
+mod1<-lm(log_ER~Agr, data=met_sub)
+summary(mod1)
+anova(mod1)
+
+mod2<-lm(log_ER~Agr+log_light+log_SRP+DIN, data=met_sub)
+anova(mod2)
+
+mod3<-lm(log_DOC~Agr+light+log_SRP, data=met_sub)
+anova(mod3)
